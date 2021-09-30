9776a032a5a3e8f9edba58933d1f2dcd1132ce0c
Nikolai Nikolaev <niki_nik_74@abv.bg>
2021-09-30 10:20:59 +0300

    Update index.js
    Require routes

--- index.js
+++ index.js
@@ -2,10 +2,12 @@ const express = require('express');
 const app = express();
 
 const portConfig = require('./config/configPort');
+const routes = require('./routes');
 
-app.get('/', (req, res) => {
-    res.send('<h1>Hello Express</h1>')
-});
+// app.get('/', (req, res) => {
+//     res.send('<h1>Hello Express</h1>')
+// });
 
+routes(app);
 
 app.listen(portConfig.development.PORT, () => console.log(`Server listening on port: ${portConfig.development.PORT}...`));
