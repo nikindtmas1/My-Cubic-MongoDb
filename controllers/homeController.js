c31d737d59009cac4958c203200c7c0ba61e1ef3
Nikolai Nikolaev <niki_nik_74@abv.bg>
2021-09-30 11:01:18 +0300

    Update homeController.js
    Change logic render

--- controllers/homeController.js
+++ controllers/homeController.js
@@ -2,7 +2,7 @@ const express = require('express');
 const router = express.Router();
 
 router.get('/', (req, res) => {
-    res.send('<h1>Hello Express</h1>');
+    res.render('index');
 });
 
 module.exports = router;
