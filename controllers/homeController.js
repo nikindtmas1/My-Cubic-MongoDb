8feeceea414c3ed6ec136a8bf4ec3d50183fee00
Nikolai Nikolaev <niki_nik_74@abv.bg>
2021-09-30 11:14:15 +0300

    Update homeController.js
    Added about page

--- controllers/homeController.js
+++ controllers/homeController.js
@@ -2,7 +2,11 @@ const express = require('express');
 const router = express.Router();
 
 router.get('/', (req, res) => {
-    res.render('index');
+    res.redirect('/cubics');
+});
+
+router.get('/about', (req, res) => {
+    res.render('about');
 });
 
 module.exports = router;
