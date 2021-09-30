9d839f4c97a1be29c84ba8ed691af56f21eba26f
Nikolai Nikolaev <niki_nik_74@abv.bg>
2021-09-30 11:01:37 +0300

    Update cubeController.js
    Change path and render

--- controllers/cubeController.js
+++ controllers/cubeController.js
@@ -1,8 +1,8 @@
 const express = require('express');
 const router = express.Router();
 
-router.get('/add-cube', (req, res) => {
-    res.send('<h1>Add Cube Page</h1>');
+router.get('/create', (req, res) => {
+    res.render('create');
 });
 
 module.exports = router;
