b9937ceae5f7f4c2ba9a0bc0fa456f3ed88bb1c9
Nikolai Nikolaev <niki_nik_74@abv.bg>
2021-09-30 09:44:21 +0300

    Update index.js
    Added port to server

--- index.js
+++ index.js
@@ -1,9 +1,11 @@
 const express = require('express');
 const app = express();
 
+const portConfig = require('./config/configPort');
+
 app.get('/', (req, res) => {
     res.send('<h1>Hello Express</h1>')
 });
 
 
-app.listen(5000, console.log('Server listening on port 5000'))
+app.listen(portConfig.development.PORT, () => console.log(`Server listening on port: ${portConfig.development.PORT}...`));
