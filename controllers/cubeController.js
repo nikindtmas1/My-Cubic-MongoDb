66c093a3a19629b4d6d6c8e6bba34441444bd8b9
Nikolai Nikolaev <niki_nik_74@abv.bg>
2021-09-30 11:14:38 +0300

    2
    Added 404 page

--- controllers/cubeController.js
+++ controllers/cubeController.js
@@ -1,8 +1,17 @@
 const express = require('express');
 const router = express.Router();
 
+router.get('/', (req, res) => {
+    res.render('index');
+});
+
 router.get('/create', (req, res) => {
     res.render('create');
+});
+
+
+router.get('*', (req, res) => {
+    res.render('404');
 });
 
 module.exports = router;
--- routes.js
+++ routes.js
@@ -6,4 +6,5 @@ module.exports = (app) => {
 
     app.use('/', homePage);
     app.use('/cubics', cubController);
+    
 }
