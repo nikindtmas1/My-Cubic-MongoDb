52837e6e1d71e1fe5761a30bc1498683f6dfa886
Nikolai Nikolaev <niki_nik_74@abv.bg>
2021-10-02 15:15:11 +0300

    Change cube model structure

--- models/cubic.js
+++ models/cubic.js
@@ -1,10 +1,26 @@
 const mongoose = require('mongoose');
 
 const cubicSchema = new mongoose.Schema({
-    name: String,
-    imageUrl: String,
-    description: String,
-    difficultyLevel: Number
+    name: {
+        type: String,
+        required: true
+    },
+    imageUrl: {
+        type: String,
+        required: true,
+        validate: /^https?/
+    },
+    description: {
+        type: String,
+        required: true,
+        maxlength: 100
+    },
+    difficultyLevel: {
+        type: Number,
+        required: true,
+        min: 1,
+        max: 6
+    }
 });
 
 module.exports = mongoose.model('Cube', cubicSchema);
