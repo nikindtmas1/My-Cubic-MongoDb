0433f0fa193806c9f9579e27b37d0c4f5f15bbd0
Nikolai Nikolaev <niki_nik_74@abv.bg>
2021-10-02 17:08:55 +0300

    Create post logic accessory

--- controllers/accessoryController.js
+++ controllers/accessoryController.js
@@ -1,9 +1,19 @@
 const express = require('express');
 const router = express.Router();
-
+const accessoryService = require('../srevices/accessoryService');
 
 router.get('/create/accessory', (req, res) => {
     res.render('createAccessory');
+});
+
+router.post('/create/accessory', (req, res) => {
+    let data = req.body;
+
+    accessoryService.createAcces(data);
+});
+
+router.get('/attach/accessory/cubId', (req, res) => {
+    res.render('attachAccessory');
 });
 
 module.exports = router;
