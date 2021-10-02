635a767d162223a9556ee15c22f2aa4b58544d60
Nikolai Nikolaev <niki_nik_74@abv.bg>
2021-10-02 15:51:36 +0300

    Added save logic

--- controllers/cubeController.js
+++ controllers/cubeController.js
@@ -15,6 +15,14 @@ router.get('/create', (req, res) => {
     res.render('create');
 });
 
+router.post('/create', cubeValide, (req, res) => {
+    let data = req.body;
+
+    cubeService.create(data);
+
+    res.redirect('/cubics');
+});
+
 router.get('/details/:cubId', async (req, res) => {
     let result = await cubeService.getOne(req.params.cubId);
 
@@ -24,5 +32,20 @@ router.get('/details/:cubId', async (req, res) => {
 router.get('*', (req, res) => {
     res.render('404');
 });
+
+function cubeValide(req, res, next){
+    let isValid = true;
+
+    if(req.body.name.trim().length < 2){
+        isValid = false;
+    }else if(!req.body.imageUrl){
+        isValid = false;
+    }
+
+    if(isValid){
+        next();
+    }
+
+}
 
 module.exports = router;
