388cd085febdf800500397acb8a057a496ad4bfa
Nikolai Nikolaev <niki_nik_74@abv.bg>
2021-10-02 17:41:14 +0300

    Change path create accessory

--- controllers/accessoryController.js
+++ controllers/accessoryController.js
@@ -2,14 +2,16 @@ const express = require('express');
 const router = express.Router();
 const accessoryService = require('../srevices/accessoryService');
 
-router.get('/create/accessory', (req, res) => {
+router.get('/create', (req, res) => {
     res.render('createAccessory');
 });
 
-router.post('/create/accessory', (req, res) => {
+router.post('/accessories/create', (req, res) => {
     let data = req.body;
-
+    console.log(data);
     accessoryService.createAcces(data);
+
+    res.redirect('/')
 });
 
 router.get('/attach/accessory/cubId', (req, res) => {
