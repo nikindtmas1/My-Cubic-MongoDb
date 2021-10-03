1fbaf205be749418a66eaeb490df73a06d3d3d93
Nikolai Nikolaev <niki_nik_74@abv.bg>
2021-10-03 14:41:34 +0300

    Added type id in accessorySchema

--- models/accessory.js
+++ models/accessory.js
@@ -1,6 +1,7 @@
 const mongoose = require('mongoose');
 
 const accessorySchema = new mongoose.Schema({
+    id: mongoose.Schema.Types.ObjectId,
     name:{
         type: String,
         required: true
