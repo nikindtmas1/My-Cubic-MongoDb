41a7aa239d2449e9b8387263fdc2bfa7c211d17e
Nikolai Nikolaev <niki_nik_74@abv.bg>
2021-10-15 10:18:40 +0300

    Added async

--- middlewares/authCubeMiddleware.js
+++ middlewares/authCubeMiddleware.js
@@ -1,12 +1,12 @@
 const cubeService = require('../srevices/cubeService');
 
-function isOwnCube(req, res, next){
+async function isOwnCube(req, res, next){
 
-    let cube = cubeService.getOne(req.params.cubId);
-
+    let cube = await cubeService.getOne(req.params.cubId);
+    
     if(cube.userId == req.user._id){
         req.cube = cube;
-
+        
         next();
     }else{
         next('You are not authorized to edit this cube!');
