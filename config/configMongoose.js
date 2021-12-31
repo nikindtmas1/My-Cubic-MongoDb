ea313c1a60b04ce77b9e708a6f992b65571e3125
Nikolai Nikolaev <niki_nik_74@abv.bg>
2021-12-31 11:16:10 +0200

    Install packages

--- config/configMongoose.js
+++ config/configMongoose.js
@@ -1,5 +1,6 @@
 const mongoose = require('mongoose');
-const connectStr = 'mongodb://localhost:27017/cubicle';
+//const connectStr = 'mongodb://localhost:27017/cubicle';
+const connectStr = 'mongodb+srv://cluster0.3bvrs.mongodb.net/myFirstDatabase?authSource=%24external&authMechanism=MONGODB-X509&retryWrites=true&w=majority';
 
 module.exports = (app) => {
 
